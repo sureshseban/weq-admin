ce21d3a3536fbc4228aa54397a1d51af02474a7c
sureshseban <suresh.vgknair@gmail.com>
2020-09-20 22:17:46 +0530

    initial commit for Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,36 @@ function Home(props) {
                     weqid: 'WQ123',
                     noBookings: '1',
                     timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL01',
+                    weqid: 'WQ123',
+                    noBookings: '10',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL02',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL03',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL02',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL03',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
                 }
             ]
         },
@@ -34,23 +64,52 @@ function Home(props) {
                     slotid: 'SL01',
                     weqid: 'WQ123',
                     noBookings: '10',
-                    timing: '11.00 AM - 11.15 AM'
+                    timing: '10.00 AM - 10.15 AM'
                 },
                 {
                     slotid: 'SL02',
                     weqid: 'WQ123',
                     noBookings: '1',
-                    timing: '11.15 AM - 11.30 AM'
+                    timing: '10.00 AM - 10.15 AM'
                 },
                 {
                     slotid: 'SL03',
                     weqid: 'WQ123',
                     noBookings: '1',
-                    timing: '11.15 AM - 11.30 AM'
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL01',
+                    weqid: 'WQ123',
+                    noBookings: '10',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL02',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL03',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL02',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
+                },
+                {
+                    slotid: 'SL03',
+                    weqid: 'WQ123',
+                    noBookings: '1',
+                    timing: '10.00 AM - 10.15 AM'
                 }
             ]
         }
-
     ])
 
     return (
@@ -59,18 +118,23 @@ function Home(props) {
             <div className='slots-section'>
                 {
                     slots.map((slot, index) => {
-                        return (<div key={index}>
-                            <h1>{slot.timing}</h1>
-                            {
-                                slot.data.map((item, _index) => {
-                                    return (<div key={_index}>
-                                        <div><span>{item.slotid}</span>,
-                                        <span>{item.weqid}</span>,
-                                        <span>{item.noBookings}</span></div>
-                                    </div>)
-                                })
-                            }
-                        </div>)
+                        return (
+                            <React.Fragment key={index}>
+                                <div className='slots-heading'>
+                                    <span className='booked-timing'>10.00 AM</span>
+                                    <span className='booked-count'>Booked 20/35</span>
+                                </div>
+                                <div className='slots-grid' key={index}>
+                                    {
+                                        slot.data.map((item, _index) => {
+                                            return (
+                                                <div key={_index} className='grid-item'></div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </React.Fragment>
+                        )
                     })
                 }
             </div>
