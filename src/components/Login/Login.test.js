6008f5a3fdb31391ccac85a6f40f48603df45cda
sureshseban <suresh.vgknair@gmail.com>
2020-10-23 10:48:42 +0530

    test suit added for login anf app

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+import { BrowserRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<BrowserRouter><Login /></BrowserRouter>, div)
+})
+
